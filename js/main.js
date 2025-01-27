73decd2e5af6a8fe4bef1311cd055387c408aaaa
digonta-roy <74439375+digonta-roy@users.noreply.github.com>
2025-01-27 16:06:57 +0600

    added

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+$('.trail').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    navText:['<','>'],
+    autoplay: true,
+    autoplayTimeout:3000,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+$('.topp').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navText:['<','>'],
+    autoplay: true,
+    autoplayTimeout:1500,
+    autoplayHoverPause: true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+$(document).ready(function() {
+    $('.popup-gallery').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+            titleSrc: function(item) {
+                return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+            }
+        }
+    });
+});
